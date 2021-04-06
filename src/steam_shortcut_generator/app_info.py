5a8e5f57f6c08c101d47788e23700a69b4f62f84
Ashleigh Udoh <ash@audoh.co.uk>
2021-04-06 01:00:42 +0100

    restructured app_info

--- src/steam_shortcut_generator/app_info.py
+++ src/steam_shortcut_generator/app_info.py
@@ -1,19 +1,31 @@
 from dataclasses import dataclass
 from typing import Dict, Optional, Tuple
 
-
-def read_bytes(data: bytes, count: int, pos: int = 0) -> Tuple[bytes, int]:
-    return data[pos : pos + count], pos + count
-
-
-def read_uint32(data: bytes, pos: int = 0) -> Tuple[int, int]:
-    _bytes, pos = read_bytes(data, count=4, pos=pos)
-    return int.from_bytes(_bytes, byteorder="little", signed=False), pos
-
-
 _FIELD_APP_ID = b"\x02appid\x00"
 _FIELD_CLIENT_ICON = b"\x01clienticon\x00"
 _FIELD_NAME = b"\x01name\x00"
+
+
+def _read_bytes(data: bytes, count: int, pos: int = 0) -> Tuple[bytes, int]:
+    return data[pos : pos + count], pos + count
+
+
+def _read_uint32(data: bytes, pos: int = 0) -> Tuple[int, int]:
+    _bytes, pos = _read_bytes(data, count=4, pos=pos)
+    return int.from_bytes(_bytes, byteorder="little", signed=False), pos
+
+
+def _read_key_uint32(data: bytes, key: bytes, pos: int = 0) -> Tuple[int, int]:
+    idx = data.index(key, pos)
+    pos = idx + len(key)
+    return _read_uint32(data, pos)
+
+
+def _read_key_utf8(data: bytes, key: bytes, pos: int = 0) -> Tuple[str, int]:
+    idx = data.index(key, pos)
+    start = idx + len(key)
+    end = data.index(b"\0", start)
+    return data[start:end].decode("utf-8"), end
 
 
 @dataclass
@@ -23,37 +35,24 @@ class App:
     icon_hash: Optional[str] = None
 
 
-def read_key_uint32(data: bytes, key: bytes, pos: int = 0) -> Tuple[int, int]:
-    idx = data.index(key, pos)
-    pos = idx + len(key)
-    return read_uint32(data, pos)
-
-
-def read_key_utf8(data: bytes, key: bytes, pos: int = 0) -> Tuple[str, int]:
-    idx = data.index(key, pos)
-    start = idx + len(key)
-    end = data.index(b"\0", start)
-    return data[start:end].decode("utf-8"), end
-
-
 def read(data: bytes) -> Dict[str, App]:
     res: Dict[str, App] = {}
     pos = 0
     while True:
         try:
-            id, pos = read_key_uint32(data, _FIELD_APP_ID, pos=pos)
+            id, pos = _read_key_uint32(data, _FIELD_APP_ID, pos=pos)
         except Exception:
             break
 
         app = App(id=id)
 
         try:
-            app.name, _ = read_key_utf8(data, key=_FIELD_NAME, pos=pos)
+            app.name, _ = _read_key_utf8(data, key=_FIELD_NAME, pos=pos)
         except ValueError:
             pass
 
         try:
-            app.icon_hash, _ = read_key_utf8(data, key=_FIELD_CLIENT_ICON, pos=pos)
+            app.icon_hash, _ = _read_key_utf8(data, key=_FIELD_CLIENT_ICON, pos=pos)
         except ValueError:
             pass
 
